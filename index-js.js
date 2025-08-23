6bf5850ce0f3de7a129dbd08c258d2cae3d63d76
Mansour Mbengue <mbgmansour13@gmail.com>
2025-08-23 16:23:56 +0200

    feat : implement wallet connection functionality with MetaMask

--- index-js.js
+++ index-js.js
@@ -1,3 +1,20 @@
-document.getElementById("connect-button").addEventListener("click", () => {
-  console.log("Connect button clicked");
-}); 
+import { createWalletClient, custom } from 'https://esm.sh/viem'
+
+const connectButton = document.getElementById("connect-button");
+
+let walletClient;
+
+async function connectWallet() {
+  if (typeof window.ethereum !== "undefined") {
+    console.log("Connect button clicked");
+    walletClient =createWalletClient({
+      transport: custom(window.ethereum)
+    });
+    await walletClient.requestAddresses();
+    connectButton.innerHTML = 'Connected';
+  } else {
+    connectButton.innerHTML = "Please install MetaMask";
+  }
+}
+
+connectButton.onclick = connectWallet;
